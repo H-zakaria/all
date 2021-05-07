79d9da60c382439448c77717836472e789db9ca5
H-zakaria <hamdi.zakaria@protonmail.com>
2021-05-07 15:30:44 +0200

    la_suite

--- includes/supr_service.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once '../header.php';
-
-$noserv = $_GET['noserv'];
-$sql = "DELETE FROM serv WHERE Noserv = '$noserv';";
-maQuery($sql, 'nop');
-header("Location: ../tableau-connecte.php?suppression=succes");
